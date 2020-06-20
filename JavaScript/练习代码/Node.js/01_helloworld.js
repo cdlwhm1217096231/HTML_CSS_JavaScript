2d08c2cdbb56fbd1b0d445c3cdd42bc65735c9b0
cdl <1217096231@qq.com>
2020-06-20 22:50:18 +0800

    Node.js快速入门

---
+++ JavaScript/练习代码/Node.js/01_helloworld.js
@@ -0,0 +1,13 @@
+var first = 'hello nodejs';
+console.log(first);
+
+const fn = () => console.log('fn函数被调用了');
+fn();
+
+for (var i = 0; i < 3; i++){
+    console.log(i);
+}
+
+if (true) {
+    console.log('123');
+}
