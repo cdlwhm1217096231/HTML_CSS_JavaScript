98b2334061feb43f97d84bdc17a91a32c34712bd
cdl <1217096231@qq.com>
2020-06-24 11:51:38 +0800

    08_global.js

---
+++ JavaScript/练习代码/Node.js/08_global.js
@@ -0,0 +1,11 @@
+// global.console.log('我是global对象下面的console.log()方法输出的内容');
+
+// global.setTimeout(() => {
+//     console.log('123');
+// }, 2000);
+
+console.log('我是global对象下面的console.log()方法输出的内容');
+
+setTimeout(() => {
+    console.log('123');
+}, 2000);
